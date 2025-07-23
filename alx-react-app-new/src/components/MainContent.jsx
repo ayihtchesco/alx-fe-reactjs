23f169ea8b9deb8f0feaa111f547f79553d4855e
Tchesco Ayih <atchesco@gmail.com>
2025-07-23 13:57:14 +0000

    Assignement 0

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main
+      style={{ padding: "20px", backgroundColor: "#e0f7fa", fontSize: "18px" }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
