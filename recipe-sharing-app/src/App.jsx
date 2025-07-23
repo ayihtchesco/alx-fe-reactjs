e333a69e5d5e5eba058d01460e36a2dce8390ae0
Tchesco Ayih <atchesco@gmail.com>
2025-07-23 15:18:14 +0000

    Assignement 01

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
