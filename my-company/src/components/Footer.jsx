d06565ee4c024066887ac934a1a630dd0eabed19
Tchesco Ayih <atchesco@gmail.com>
2025-07-23 14:47:57 +0000

    Assignement 3

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#222",
+        color: "white",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
