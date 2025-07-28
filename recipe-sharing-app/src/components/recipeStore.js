3b8bf6195b070cad8b9639edb2f4db5d59750996
Tchesco Ayih <atchesco@gmail.com>
2025-07-28 11:28:55 +0000

    Implement User Favorites and Personalized Recipe Recommendations in recipe-sharing-app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -10,6 +10,17 @@ const useRecipeStore = create((set) => ({
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
   // Add to favorites
   addFavorite: (recipeId) =>
     set((state) => ({
