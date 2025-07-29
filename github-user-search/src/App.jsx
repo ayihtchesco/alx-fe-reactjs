0fa07a1a519244a16c15995fbf77ac0c6224844b
Tchesco Ayih <atchesco@gmail.com>
2025-07-29 08:53:04 +0000

    adding first assignement

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start building your search component here.</p>
+    </div>
+  );
+}
+
+export default App;
