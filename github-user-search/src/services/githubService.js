5152b443d82095eaf67164e5330199ce5204922a
Tchesco Ayih <atchesco@gmail.com>
2025-07-29 09:53:40 +0000

    adding assignement 2

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
-
 export const fetchUserData = async ({ username, location, minRepos }) => {
   let query = "";
 
@@ -10,7 +8,7 @@ export const fetchUserData = async ({ username, location, minRepos }) => {
   if (minRepos) query += `repos:>=${minRepos}`;
 
   const response = await axios.get(
-    `${BASE_URL}/search/users?q=${encodeURIComponent(query.trim())}`
+    `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}`
   );
   return response.data.items;
 };
