2c4fe9f32677a8a94750c0537ea9156170d7c697
Tchesco Ayih <atchesco@gmail.com>
2025-08-23 11:18:06 +0000

    Assignment 2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,12 +4,12 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import BlogPost from "./pages/BlogPost";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
