7082cfe4d5649fc95796ab44b6bc9fbe6c652941
Tchesco Ayih <atchesco@gmail.com>
2025-08-23 11:28:11 +0000

    Assignment 2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -26,5 +27,17 @@ function App() {
     </Router>
   );
 }
+
+<Routes>
+  <Route
+    path="/profile/*"
+    element={
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
+  <Route path="/blog/:id" element={<BlogPost />} />
+</Routes>;
 
 export default App;
